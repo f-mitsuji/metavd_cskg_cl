6ae31d52c364058e1d8a66090ddecf8fe9429cb7
Fumiya Mitsuji <fumiya.mitsuji@gmail.com>
2024-08-01 18:07:31 +0900

    fix: Corrected issues causing tests to fail

--- tests/test_lemmatize_labels.py
+++ tests/test_lemmatize_labels.py
@@ -1,16 +1,14 @@
 import pytest
 
-from src.lemmatize_labels import lemmatize_and_remove_stopwords, preprocess_label, process_labels
+from src.lemmatize_labels import lemmatize_and_filter_token, lemmatize_labels, normalize_label
 
-# テストデータを定義
 test_labels = ["running_fast", "jumping_high"]
-expected_processed_labels = {"running_fast": ["run", "fast"], "jumping_high": ["jump", "high"]}
+expected_lemmatized_labels = {"running_fast": ["run", "fast"], "jumping_high": ["jump", "high"]}
 
 
-def test_preprocess_label():
-    """Test the preprocess_label function."""
-    assert preprocess_label("Running_Fast") == "running fast"
-    assert preprocess_label("Jumping-High") == "jumping high"
+def test_normalize_label():
+    assert normalize_label("Running_Fast") == "running fast"
+    assert normalize_label("Jumping-High") == "jumping high"
 
 
 @pytest.mark.parametrize(
@@ -20,11 +18,9 @@ def test_preprocess_label():
         ("jumping high", ["jump", "high"]),
     ],
 )
-def test_lemmatize_and_remove_stopwords(label, expected):
-    """Test the lemmatize_and_remove_stopwords function."""
-    assert lemmatize_and_remove_stopwords(label) == expected
+def test_lemmatize_and_filter_token(label, expected):
+    assert lemmatize_and_filter_token(label) == expected
 
 
-def test_process_labels():
-    """Test the process_labels function."""
-    assert process_labels(test_labels) == expected_processed_labels
+def test_lemmatize_labels():
+    assert lemmatize_labels(test_labels) == expected_lemmatized_labels
