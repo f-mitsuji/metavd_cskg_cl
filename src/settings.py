1c04752aba4ea8718e58e3374103a0e6ec539795
Fumiya Mitsuji <fumiya.mitsuji@gmail.com>
2024-09-19 10:19:04 +0900

    add: load stop_words

--- src/settings.py
+++ src/settings.py
@@ -14,3 +14,13 @@ CN_DICT_DIR = DATA_DIR / "conceptnet"
 CN_EN_LEMMAS_P = CN_DICT_DIR / "concepts_en_lemmas.p"
 CN_DICT2_P = CN_DICT_DIR / "cn_dict2.p"
 CN_DICT2_DB = CN_DICT_DIR / "cn_dict2.db"
+
+STOP_WORDS_FILE_PATH = DATA_DIR / "stop_words.txt"
+
+
+def load_stop_words(file_path: Path) -> set[str]:
+    with file_path.open(encoding="utf-8") as file:
+        return {line.strip() for line in file if line.strip() and not line.startswith("#")}
+
+
+STOP_WORDS = load_stop_words(STOP_WORDS_FILE_PATH)
