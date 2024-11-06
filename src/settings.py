65b9375a99ef08be9bddd5b2e867e60d1e8a2292
Fumiya Mitsuji <fumiya.mitsuji@gmail.com>
2024-11-07 00:19:58 +0900

    feat: Add directories for models and video datasets

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,14 @@ RESULTS_DIR = Path("results")
 LOGS_DIR = RESULTS_DIR / "logs"
 
 DATA_DIR = Path("data")
+
 METAVD_DIR = DATA_DIR / "metavd"
+
+MODELS_DIR = Path("models")
+TRAINED_MODELS_DIR = MODELS_DIR / "trained"
+CHECKPOINTS_DIR = MODELS_DIR / "checkpoints"
+
+SENT2VEC_MODEL_PATH = Path("sent2vec/models/wiki_unigrams.bin")
 
 EMBEDDINGS_DIR = DATA_DIR / "embeddings"
 NUMBERBATCH_PATH = EMBEDDINGS_DIR / "numberbatch-en-19.08.txt"
@@ -14,6 +21,14 @@ CN_DICT_DIR = DATA_DIR / "conceptnet"
 CN_EN_LEMMAS_P = CN_DICT_DIR / "concepts_en_lemmas.p"
 CN_DICT2_P = CN_DICT_DIR / "cn_dict2.p"
 CN_DICT2_DB = CN_DICT_DIR / "cn_dict2.db"
+
+VIDEO_DATASET_DIR = Path("/disk1")
+CHARADES_DIR = VIDEO_DATASET_DIR / "charades"
+KINETICS700_DIR = VIDEO_DATASET_DIR / "kinetics700"
+STAIR_ACTIONS_DIR = VIDEO_DATASET_DIR / "stair_actions"
+ACTIVITYNET_DIR = VIDEO_DATASET_DIR / "activitynet"
+HMDB51_DIR = VIDEO_DATASET_DIR / "hmdb51"
+UCF101_DIR = VIDEO_DATASET_DIR / "ucf101"
 
 STOP_WORDS_FILE_PATH = DATA_DIR / "stop_words.txt"
 
